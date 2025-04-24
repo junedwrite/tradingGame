db9298cff9e88b523d8443cd56e3683abbb4fa25
juned <junedwrites@gmail.com>
2025-04-24 21:40:56 +0530

    server changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
+  // base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
+  base:'tradingGame',
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
